c07c54aff562a550f5466894dd7ad3d08fb8ace8
PCLZ550NSB\博子 <o0h.ishida0o@gmail.com>
2022-12-14 17:47:51 +0900

    自分のプロフィール編集画面、images以外更新可能

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -32,16 +32,29 @@ class UsersController extends \App\Http\Controllers\Controller
         $request->validate([
             'username' => 'required|string|min:4|max:12',
             'mail' => ['required', 'string', 'email', 'min:4', 'max:12', Rule::unique('users')->ignore(Auth::id())],
-            'password' => 'required', 'min:4', 'max:12',
+            'password2' => 'required', 'min:4',
             'bio' => 'string|max:200|nullable',
-            'images' => 'file|nullable',
-        ]);
+            'images' => 'nullable',
+        ], [
+        'username.required' => 'ユーザー名は必須項目です',
+        'username.min' => 'ユーザー名は4文字以上で入力してください',
+        'username.max' => 'ユーザー名は12文字以内で入力してください',
+		'mail.required' => 'メールアドレスは必須項目です',
+		'mail.email' => 'メールアドレスではありません',
+        'mail.min' => 'メールアドレスは4文字以上で入力してください',
+        'mail.max' => 'メールアドレスは12文字以内で入力してください',
+        'mail.unique' => 'このメールアドレスは既に使われています。',
+		'password2.required' => 'パスワードは必須項目です',
+		'password2.min' => 'パスワードは4文字以上で入力してください',
+        'password2.alpha_num' => 'パスワードは半角英数字で入力してください',
+        'bio.max' => '自己紹介文は200文字以内で入力してください',
+	]);
 
-        if(!isset($image)){
+        if(!isset($user->images)){
             $user = Auth::user();
             $user->username = $request->input('username');
             $user->mail = $request->input('mail');
-            $user->password = bcrypt($request->input('password'));
+            $user->password = bcrypt($request->input('password2'));
             $user->bio = $request->input('bio');
             $user->save();
 
@@ -49,7 +62,9 @@ class UsersController extends \App\Http\Controllers\Controller
             $user = Auth::user();
             $user->username = $request->input('username');
             $user->mail = $request->input('mail');
+            $user->password = bcrypt($request->input('password2'));
             $user->bio = $request->input('bio');
+            $user->images = $request->input('images');
             $user->save();
         }
             return redirect()->route('profile_edit')->with('msg_success', 'プロフィールの更新が完了しました');
--- resources/views/posts/profile.blade.php
+++ resources/views/posts/profile.blade.php
@@ -10,20 +10,34 @@
          </div>
                 <input type="hidden" name="id" value="{{ $user->id }}" />
          <div class="edit-content">
-                <span class="edit-username">UserName</span>
-                <input type="text" name="username" value="{{ $user->username }}" /><br><span class="edit-mail">MailAdress</span>
-                <input type="text" name="mail" value="{{ $user->mail }}" /><br><span class="edit-password">Password</span>
-                <input type="password" name="password1" value="{{ $user->password }}" readonly /><br><span class="edit-password">new Password</span>
-                <input type="password" name="password2" value="{{ $user->password }}" /><br>
+                <span class="edit-username">UserName</span><input type="text" name="username" value="{{ $user->username }}" /><br>
+                @if ($errors->has('username'))
+                <p>{{$errors->first('username')}}</p>
+                @endif
+
+                <span class="edit-mail">MailAdress</span><input type="text" name="mail" value="{{ $user->mail }}" /><br>
+                @if ($errors->has('mail'))
+                <p>{{$errors->first('mail')}}</p>
+                @endif
+
+                <span class="edit-password">Password</span><input type="password" name="password1" value="{{ $user->password }}" readonly /><br>
+                <span class="edit-password">new Password</span><input type="password" name="password2" value="{{ $user->password }}" /><br>
+                @if ($errors->has('password2'))
+                <p>{{$errors->first('password2')}}</p>
+                @endif
+
                 <div class="form-bio">
                         <span class="edit-bio">Bio</span>
-                        <textarea name="bio" rows="3" value="{{ $user->bio }}"></textarea><br>
+                        <textarea name="bio" rows="3">{{ $user->bio }}</textarea><br>
+                        @if ($errors->has('bio'))
+                        <p>{{$errors->first('bio')}}</p>
+                        @endif
                 </div>
                 <div class="form-images">
                         <span class="edit-images">Icon Image</span>
                                 <div class="choice-images">
                                         <label class="label-images">
-                                        <input type="file" name="images"value="{{ $user->images }}">ファイルを選択</label>
+                                        <input type="file" name="images">ファイルを選択</label>
                                 </div><br>
                 </div>
                 <div class="update-button">
